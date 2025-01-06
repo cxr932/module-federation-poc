f9fc2f0cc91df04c440ce861c9f836e3818765dc
Husnain Tariq <hxan619@gmail.com>
2025-01-07 04:32:33 +0500

    Initial commit
    To connect your workspace to Nx Cloud, push your repository
    to your git hosting provider and go to the following URL:
    
    https://cloud.nx.app/connect/nbHPmbuUQG

---
+++ apps/microfrontend-nx-project/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'microfrontend-nx-project',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/microfrontend-nx-project',
+};
